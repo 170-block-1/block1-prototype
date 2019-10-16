e5cd94832917d0b04aa32009b3498669c2c52d7d
Valentino Abate <valentinoabate1@gmail.com>
2019-10-15 18:31:09 -0700

    Basic Scavenging System 2
    - Added a debug display and a continue button

--- Prototype/170Block1Prototype/Assets/Scripts/Scavenging/ScavengeManager.cs
+++ Prototype/170Block1Prototype/Assets/Scripts/Scavenging/ScavengeManager.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
 
 [RequireComponent(typeof(Scavenger))]
 public class ScavengeManager : MonoBehaviour
 {
+    public int maxTries;
     public LootTable lootTable;
+    [Header("Control Button References")]
+    public Button scavengeButton;
+    public Button continueButton;
+    [Header("Display Text References")]
+    public Text displayText;
+    public Text energyText;
+    public Text inventoryText;
     private Scavenger scav;
+    private int triesLeft;
+    private int TriesLeft
+    {
+        get => triesLeft;
+        set
+        {
+            triesLeft = value;
+            energyText.text = "Energy: " + triesLeft;
+        }
+    }
 
     private void Awake()
     {
         scav = GetComponent<Scavenger>();
+        TriesLeft = maxTries;
+        displayText.text = "Click the button to scavenge";
+        continueButton.gameObject.SetActive(false);
+        inventoryText.text = "Inventory:\n";
     }
     public void StartScavenge()
     {
-        Debug.Log("The player scavenged: " + scav.Scavenge(lootTable).name);
+        var item = scav.Scavenge(lootTable);
+        if (--TriesLeft <= 0)
+        {
+            scavengeButton.interactable = false;
+            continueButton.gameObject.SetActive(true);
+            inventoryText.text += item.name;
+            displayText.text = "You found: " + item.name + " x1.\n You are out of energy!";
+        }
+        else if (item.name == TempItem.nothing)
+        {
+            displayText.text = "You found: nothing. Better luck next time!";
+        }
+        else
+        {
+            inventoryText.text += item.name + ", ";
+            displayText.text = "You found: " + item.name + " x1";
+        }
     }
 }
--- Prototype/170Block1Prototype/Assets/Scripts/Scavenging/TempItem.cs
+++ Prototype/170Block1Prototype/Assets/Scripts/Scavenging/TempItem.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class TempItem
 {
+    public static readonly string nothing = "Nothing";
     public string name;
 }
