dcd857e61ec6f380197fe62e204c07716fa5d6f1
Jeanette <jeanettemui101@gmail.com>
2019-10-18 16:47:30 -0700

    Dialogue System + Prefab

--- Prototype/170Block1Prototype/Assets/Scripts/Dialogue.cs
+++ Prototype/170Block1Prototype/Assets/Scripts/Dialogue.cs
@@ -1,33 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
-public class Dialogue : MonoBehaviour
+[System.Serializable]
+
+// For creating more dialoguein unity 
+public class Dialogue
 {
-    // Decalre text component for dialogue
-    Text dialogue;
-    private string[] text;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Get the canvas's current text component
-        dialogue = GetComponent<Text>();
-
-        // Initialize variables
-        text = new string[]{"1", "2", "3", "4", "5"};
-
-        // Move to update later
-        // Greeting -> Request
-        // Update the current text
-        dialogue.text = "testing: " + text[Random.Range(0, text.Length)];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Click to cycle through diaglue
-
-    }
+    [TextArea(3,10)]
+    public string[] sentences;
 }
---
+++ Prototype/170Block1Prototype/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Calls the function to start the customer dialogue
+ */
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
