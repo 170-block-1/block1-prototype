5cb670860d5841e05584e14a9fa0b89cfbb71457
aniken998 <enzhou@ucsc.edu>
2019-10-21 16:51:31 -0700

    make quest system single

--- Prototype/170Block1Prototype/Assets/Enxuan/Scripts/QuestManger.cs
+++ Prototype/170Block1Prototype/Assets/Enxuan/Scripts/QuestManger.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[DisallowMultipleComponent]
+
 public class QuestManger : MonoBehaviour
 {
     // Start is called before the first frame update
+    public static QuestManger instance = null;
     public Quest[] quests;
 
     public Text questName;
@@ -14,18 +17,35 @@ public class QuestManger : MonoBehaviour
 
     public Text fullDiscription;
 
-    private Quest currentQuest;
-
     private Queue<Quest> questList;
-    void Start()
+    void Awake()
     {
+
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject.transform);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Debug.Log("DEBUG");
+
         questList = new Queue<Quest>();
 
-        foreach(Quest q in quests) {
+        foreach(Quest q in quests)
+        {
             questList.Enqueue(q);
         }
-        if(questList.Count > 0) {
-            currentQuest = questList.Dequeue();
+    }
+
+    void Start() 
+    {
+        if(questList.Count > 0) 
+        {
+            Player.instance.currentQuest = questList.Dequeue();
             updateQuestInfo();
         }
     }
@@ -33,8 +53,8 @@ public class QuestManger : MonoBehaviour
     // Update is called once per frame
     public void QuestComplete() {
         if(questList.Count > 0) {
-            if(currentQuest.completed){
-                currentQuest = questList.Dequeue();
+            if(Player.instance.currentQuest.completed){
+                Player.instance.currentQuest = questList.Dequeue();
             }
         }
         updateQuestInfo();
@@ -42,8 +62,8 @@ public class QuestManger : MonoBehaviour
 
     private void updateQuestInfo() 
     {
-        questName.text = currentQuest.name;
-        discription.text = currentQuest.discription;
-        fullDiscription.text = currentQuest.fullDiscription;
+        questName.text = Player.instance.currentQuest.name;
+        discription.text = Player.instance.currentQuest.discription;
+        fullDiscription.text = Player.instance.currentQuest.fullDiscription;
     }
 }
