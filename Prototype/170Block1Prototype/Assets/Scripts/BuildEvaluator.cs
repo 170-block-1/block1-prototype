d674c3416b5ced8ba5fc8aa415207055ff9400bd
Jeanette <jeanettemui101@gmail.com>
2019-10-21 23:18:08 -0700

    Quests 1 and 2

---
+++ Prototype/170Block1Prototype/Assets/Scripts/BuildEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BuildEvaluator : MonoBehaviour
+{
+    public void EvaluateBuild(List<Part> parts)
+    {
+        switch (Player.instance.currentQuest.name)
+        {
+            case "First Quest":
+                break;
+            case "Second Quest":
+                break;
+        }
+    }
+
+    //private bool EvaluateFirstQuest(List<Part> parts, out int bonus)
+    //{ 
+    //    // sucess: have a tool (failure otherwise)
+    //    // having more than one tool gives a bonus
+    //    // 
+    //}
+
+}
