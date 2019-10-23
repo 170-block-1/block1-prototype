92c49fa917059a6b82b18da349842dd7002fe922
Valentino Abate <valentinoabate1@gmail.com>
2019-10-23 02:45:37 -0700

    Evaluation function basics

--- Prototype/170Block1Prototype/Assets/Scripts/BuildEvaluator.cs
+++ Prototype/170Block1Prototype/Assets/Scripts/BuildEvaluator.cs
@@ -26,61 +26,141 @@ public class BuildEvaluator : MonoBehaviour
 
     private bool EvaluateFirstQuest(List<Part> parts, out int bonus, out Dialogue dialog)
     {
+        bonus = 0;
         var sentences = new List<string>();
-        // Does the part list have a tool?
-        bool hasTool = parts.FirstOrDefault((p) => p.partClass == Part.Class.Tool) != null;
 
         // Fail if you don't have a tool
-        if(!hasTool)
-        {
-            bonus = 0;
+        if(!parts.Any((p) => p.partClass == Part.Class.Tool))
+        {          
             sentences.Add("Your robot was a failure!");
             sentences.Add("Try adding a tool!");
             dialog = new Dialogue() { sentences = sentences.ToArray() };
             return false;
         }
-  
+        
         sentences.Add("success");
-        bool hasMonitor = ;
-        if(parts.FirstOrDefault((p) => p.DisplayName.ToLower() == "monitor") != null)
+        // Bonus and mesaage for having a monitor
+        if(parts.Any((p) => p.DisplayName.ToLower() == "monitor"))
         {
-
+            ++bonus;
+            sentences.Add("Bonus: has monitor");
         }
-        if(parts.FirstOrDefault((p) => p.DisplayName.ToLower() == "storage") != null)
+        // Bonus and mesaage for having storage
+        if (parts.Any((p) => p.DisplayName.ToLower() == "storage compartment"))
         {
-
+            ++bonus;
+            sentences.Add("Bonus: has storage");
         }
-        bool hasTreads = parts.FirstOrDefault((p) => p.DisplayName.ToLower() == "treads");
-        dialog = new Dialogue() { sentences = sentences.ToArray() };
-        
-        return true;
-
-        // Success: have a tool (failure otherwise)
-        // Bonus:
-        // +1, monitor, storage, treads
-        // having more than one tool gives a bonus
-
-        // Success but with no bonus
-        // Extra Successs messages: monitor, storage, treads
-        // Failure
-        
+        // Bonus and mesaage for having treads
+        if (parts.Any((p) => p.DisplayName.ToLower() == "rotary treads"))
+        {
+            ++bonus;
+            sentences.Add("Bonus: has treads");
+        }
+        sentences.Add("Your bonus was: " + bonus);
+        dialog = new Dialogue() { sentences = sentences.ToArray() };      
+        return true;        
     }
 
     private bool EvaluateSecondQuest(List<Part> parts, out int bonus, out Dialogue dialog)
     {
-        // sucess: have a tool (failure otherwise)
-        // Must have a hand
-        // Must have 4 or more points from this table
-        // +2: Treads
-        // +1: Compassion core, monitor, thermos, storage, scope
-        // Any additional points above the threshold are bonus
+        bonus = 0;
+        var sentences = new List<string>();
 
-        // Success w/ treads
-        // Success w/out treads
-        // Extra messages: no compassion core, no monitor, no thermos, no storage, no scope, 
-        // Failure due to not having hands
-        // Fail due to not having enough additional satisfaction
-        // Both failure conditions
+        // Fail if you don't have a hand
+        if (!parts.Any((p) => p.DisplayName.ToLower() == "hand"))
+        {
+            sentences.Add("Your robot was a failure!");
+            sentences.Add("Try adding something to hold objects!");
+            dialog = new Dialogue() { sentences = sentences.ToArray() };
+            return false;
+        }
+
+        int satisfaction = 0;
+
+        #region Specific Part stuff
+
+        // Treads bonus / penalty
+        if (parts.Any((p) => p.DisplayName.ToLower() == "rotary treads"))
+        {
+            satisfaction += 2;
+            sentences.Add("Your robot benefits greatly from its treads' mobility!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers greatly from its lack of mobility!");
+        }
+        // Monitor bonus / penalty
+        if(parts.Any((p) => p.DisplayName.ToLower() == "monitor"))
+        {
+            satisfaction += 1;
+            sentences.Add("Your robot benefits greatly from its vision!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers from its lack of vision!");
+        }
+
+        // Compassion core bonus / penalty
+        if (parts.Any((p) => p.DisplayName.ToLower() == "compassion core"))
+        {
+            satisfaction += 1;
+            sentences.Add("Your robot benefits from its compassion!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers from its lack of compassion!");
+        }
+
+        // Thermos bonus / penalty
+        if (parts.Any((p) => p.DisplayName.ToLower() == "thermos"))
+        {
+            satisfaction += 1;
+            sentences.Add("Your robot benefits from its temperature control components!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers from its lack of temperature control components!");
+        }
+
+        // Vital Scope bonus / penalty
+        if (parts.Any((p) => p.DisplayName.ToLower() == "vital scope"))
+        {
+            satisfaction += 1;
+            sentences.Add("Your robot benefits greatly from its ability to check vitals!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers from its lack of ability to check vitals!");
+        }
+
+        // storage core bonus / penalty
+        if (parts.Any((p) => p.DisplayName.ToLower() == "storage compartment"))
+        {
+            satisfaction += 1;
+            sentences.Add("Your robot benefits from its expanded storage!");
+        }
+        else
+        {
+            sentences.Add("Your robot suffers from its lack of storage!");
+        }
+
+        #endregion
+
+        if (satisfaction < 4)
+        {
+            bonus = 0;
+            sentences.Add("Your robot was a failure!");
+            sentences.Add("Your satisfaction wasn't high enough!");
+            dialog = new Dialogue() { sentences = sentences.ToArray() };
+            return false;
+        }
+
+        bonus = satisfaction - 4;
+        sentences.Add("Success!");
+        sentences.Add("Your bonus was: " + bonus);
+        dialog = new Dialogue() { sentences = sentences.ToArray() };
+        return true;
     }
 
 }
